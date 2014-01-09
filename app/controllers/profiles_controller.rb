fe8daac1c3d31f67789eefd098d10359446b77b1
Bernd Weber <bernd@nvisionary.com>
2014-01-08 19:14:01 -0800

    Adding better control logic for developers role

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
   def dashboard
     @user = User.find(current_user.id)
-    if @user.role? :admin
+    if (@user.role? :admin) || (@user.role? :developer)
       redirect_to admin_users_path
     else
    	  redirect_to edit_user_registration_path
