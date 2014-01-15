884790b4604031fb7da6ac6201b49bd4a5158d54
Bernd Weber <bernd@nvisionary.com>
2014-01-15 15:33:38 -0800

    Improved the profile drop down menu and added ability to choose which customer data to show

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+  	if !params[:current_customer].blank?
+  	  session[:current_customer] = params[:current_customer]
+  	end
   end
 end
